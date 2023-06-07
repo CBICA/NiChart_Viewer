bfc2c1615ed961bfe61530d7eb0fa2de93be8d24
gurayerus <gurayerus@gmail.com>
2023-06-06 20:09:42 -0400

    Minor edit in test

--- testing/tmp_test1.sh
+++ testing/tmp_test1.sh
@@ -2,7 +2,7 @@
 conda activate NiChartGUI
 pip install .
 
-ddir='/home/guraylab/AIBIL/Github/NiChartPackages/NiChartHarmonize/test_temp/Test3/outputs/EXP2_ALL_TrainTest'
+ddir='/home/guray/Github/NiChartPackages/NiChartGUI/tmp_testing/EXP2_ALL_TrainTest'
 
 # NiChartGUI --data_file ${ddir}/Set_Test.csv --data_file ${ddir}/Set_Train.csv
 # NiChartGUI --data_file ${ddir}/Set_Test.csv
