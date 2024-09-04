34d4be631631bb5d7cf55563c6b0902b05775828
gurayerus <gurayerus@gmail.com>
2024-09-04 17:06:19 -0400

    Edit

--- src/pages/1_ShowData.py
+++ src/pages/1_ShowData.py
@@ -1,7 +1,11 @@
-import streamlit as st
 import pandas as pd
-import altair as alt
-from urllib.error import URLError
+import streamlit as st
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
 st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
 
@@ -12,44 +16,84 @@ st.write(
     """
 )
 
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
 
-@st.cache_data
-def get_nichart_data():
-    fname = "../examples/test_input/vTest1/Study1/StudyTest1_DLMUSE_All.csv"
-    df = pd.read_csv(fname)
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            left.write("↳")
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    _min,
+                    _max,
+                    (_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].str.contains(user_text_input)]
+
     return df
 
+fname = "../examples/test_input/vTest1/Study1/StudyTest1_DLMUSE_All.csv"
+df = pd.read_csv(fname)
+df = df.head(40)
 
-try:
-    df = get_nichart_data()
-    dx_ad = st.multiselect(
-        "Choose DX_AD", list(df.DX_AD), ["CN", "MCI", "AD"]
-    )
-    if not dx_ad:
-        st.error("Please select at least one DX for AD.")
-    else:
-        data = df[df.DX_AD.isin(dx_ad)]
-        st.write("### NiChart data", df.head(10))
-
-        #data = data.T.reset_index()
-        #data = pd.melt(data, id_vars=["index"]).rename(
-            #columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        #)
-        #chart = (
-            #alt.Chart(data)
-            #.mark_area(opacity=0.3)
-            #.encode(
-                #x="year:T",
-                #y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                #color="Region:N",
-            #)
-        #)
-        #st.altair_chart(chart, use_container_width=True)
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+st.dataframe(filter_dataframe(df))
