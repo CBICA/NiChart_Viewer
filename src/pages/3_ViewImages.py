105eaf8c762ccf17ce90695a0af81c5ba5251071
gurayerus <gurayerus@example.com>
2024-09-08 16:39:39 -0400

    Add nichart viewer - very early version

--- src/pages/3_ViewImages.py
+++ src/pages/3_ViewImages.py
@@ -2,95 +2,42 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 from urllib.error import URLError
+import nibabel as nib
+import numpy as np
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
+st.set_page_config(page_title="Nifti Viewer Demo", page_icon="🌍")
 
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
+st.markdown("# Nifti Viewer Demo")
+st.sidebar.header("Nifti Viewer Demo")
 st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/develop/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
+    """This demo shows how to display Nifti images"""
 )
 
+st.write('Hello')
 
-@st.cache_data
-def from_data_file(filename):
-    url = (
-        "http://raw.githubusercontent.com/streamlit/"
-        "example-data/master/hello/v1/%s" % filename
-    )
-    return pd.read_json(url)
+# FIXME: Input data is hardcoded here for now
+f1 = "/home/gurayerus/GitHub/data/vTest2/Study1/Images/IXI002-Guys-0828_T1.nii.gz"
+f2 = "/home/gurayerus/GitHub/data/vTest2/Study1/DLMUSE/IXI002-Guys-0828_T1_DLMUSE.nii.gz"
+sel_roi = 51
+mask_color = (0, 255, 0)  # RGB format
 
 
-try:
-    ALL_LAYERS = {
-        "Bike Rentals": pdk.Layer(
-            "HexagonLayer",
-            data=from_data_file("bike_rental_stats.json"),
-            get_position=["lon", "lat"],
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            extruded=True,
-        ),
-        "Bart Stop Exits": pdk.Layer(
-            "ScatterplotLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_color=[200, 30, 0, 160],
-            get_radius="[exits]",
-            radius_scale=0.05,
-        ),
-        "Bart Stop Names": pdk.Layer(
-            "TextLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_text="name",
-            get_color=[0, 0, 0, 200],
-            get_size=15,
-            get_alignment_baseline="'bottom'",
-        ),
-        "Outbound Flow": pdk.Layer(
-            "ArcLayer",
-            data=from_data_file("bart_path_stats.json"),
-            get_source_position=["lon", "lat"],
-            get_target_position=["lon2", "lat2"],
-            get_source_color=[200, 30, 0, 160],
-            get_target_color=[200, 30, 0, 160],
-            auto_highlight=True,
-            width_scale=0.0001,
-            get_width="outbound",
-            width_min_pixels=3,
-            width_max_pixels=30,
-        ),
-    }
-    st.sidebar.markdown("### Map Layers")
-    selected_layers = [
-        layer
-        for layer_name, layer in ALL_LAYERS.items()
-        if st.sidebar.checkbox(layer_name, True)
-    ]
-    if selected_layers:
-        st.pydeck_chart(
-            pdk.Deck(
-                map_style="mapbox://styles/mapbox/light-v9",
-                initial_view_state={
-                    "latitude": 37.76,
-                    "longitude": -122.4,
-                    "zoom": 11,
-                    "pitch": 50,
-                },
-                layers=selected_layers,
-            )
-        )
-    else:
-        st.error("Please choose at least one layer above.")
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+nii1 = nib.load(f1)
+nii2 = nib.load(f2)
+
+img1 = nii1.get_fdata()
+img2 = nii2.get_fdata()
+
+img1 = np.rot90(img1)
+img2 = np.rot90(img2)
+
+img1 = np.stack((img1,)*3, axis=-1)
+img1[img2 == sel_roi] = mask_color
+
+img1 = img1 / img1.max()
+
+# Create a slider to select the slice index
+slice_index = st.slider("Select Slice Index", 0, img1.shape[2] - 1, value=95)
+
+# Extract the slice and display it
+st.image(img1[:, :, slice_index], width=800)
